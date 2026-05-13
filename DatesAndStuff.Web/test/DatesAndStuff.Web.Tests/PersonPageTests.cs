16c70e81712ab86c9b28e48358a7041bbf791931
Horvath Attila Levente <attihorvath13@gmail.com>
2026-05-13 16:51:37 +0300

    lab4 - 1

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -97,29 +97,38 @@ public class PersonPageTests
         Assert.That(verificationErrors.ToString(), Is.EqualTo(""));
     }
 
-    [Test]
-    public void Person_SalaryIncrease_ShouldIncrease()
+    [TestCase(5)]
+    [TestCase(10)]
+    [TestCase(20)]
+    public void Person_SalaryIncrease_ShouldIncrease(double percentage)
     {
-        // Arrange
         driver.Navigate().GoToUrl(BaseURL);
-        driver.FindElement(By.XPath("//*[@data-test='PersonPageNavigation']")).Click();
-
         var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='PersonPageNavigation']"))).Click();
+
+        var initialText = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='DisplayedSalary']"))).Text;
+        double currentSalary = double.Parse(initialText);
+
+        double expectedSalary = currentSalary * (1 + (percentage / 100));
 
         var input = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
         input.Clear();
-        input.SendKeys("5");
+        input.SendKeys(percentage.ToString());
 
-        // Act
-        var submitButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']")));
-        submitButton.Click();
+        wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']"))).Click();
 
+        wait.Until(d => {
+            var element = d.FindElement(By.XPath("//*[@data-test='DisplayedSalary']"));
+            return element.Text != initialText;
+        });
 
-        // Assert
-        var salaryLabel = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-test='DisplayedSalary']")));
-        var salaryAfterSubmission = double.Parse(salaryLabel.Text);
-        salaryAfterSubmission.Should().BeApproximately(5250, 0.001);
+        var finalSalaryText = driver.FindElement(By.XPath("//*[@data-test='DisplayedSalary']")).Text;
+        double actualSalary = double.Parse(finalSalaryText);
+
+        actualSalary.Should().BeApproximately(expectedSalary, 0.001);
     }
+
     private bool IsElementPresent(By by)
     {
         try
